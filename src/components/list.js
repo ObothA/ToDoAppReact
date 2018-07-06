2dbe442d3c6a4a4ce16adb282d203d97e43f9b59
NamulaImmaculate <namulaimm@gmail.com>
2018-07-05 23:35:58 -0700

    The modified list component

--- src/components/list.js
+++ src/components/list.js
@@ -1,16 +1,24 @@
-import React from "react";
+import { createElement } from 'react';
+
+export const List = (props) => createElement('div',{ className: "list"},
+    createElement('h5', {}, "Today's Schedule"),
+    createElement('table', { className: "table table-hover"})
 
 
+    )
 
-export default class List extends React.Component {
-    render(){
-        return(
-            <div className="list">
-                <h5>Today's Schedule</h5>
-                <table id="tasksTable" className="table table-hover">
+  
 
-                </table>
-            </div>
-        );
-    }
-}
+
+// export default class List extends React.Component {
+    // render(){
+    //    return(
+      //      <div className="list">
+               // <h5>Today's Schedule</h5>
+               // <table id="tasksTable" className="table table-hover">
+
+              //  </table>
+          //  </div>
+       // );
+   // }
+// }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import "./bootstrap/css/bootstrap.min.css";
 import './index.css';
 import { element as Banner } from "./components/banner";
-import List from "./components/list";
-import { Form } from "./components/form";
+import {List} from "./components/list";
+import Form from "./components/form";
 
 const props = {
   date: new Date().toLocaleDateString()
